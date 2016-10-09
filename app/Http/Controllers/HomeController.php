2eeb53de8dfd17e0e6a828b58e4ec787fb9b9889
Marcelo Rossini <mar.18.celo@gmail.com>
2016-10-09 19:22:50 -0300

    Ajuste na estrutura de pastas e inicio de testes de conexão com o buscapé

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class HomeController extends Controller
+{
+	public function index()
+		{
+			$teste = $this->search();
+			return view('home/index',compact('teste'));
+		}
+
+	public function search()
+		{
+			$keyword = 'Motorola Moto X primeira geração';
+			$url = file_get_contents('http://sandbox.buscape.com.br/service/findProductList/554163674d2f57624d676f3d/BR/?categoryId=77&keyword='.urlencode($keyword));
+			$xml = new \SimpleXMLElement($url);
+
+			/*
+			$xml->product[0]->productName
+			$xml->product[0]->priceMin
+			$xml->product[0]->priceMax
+			$xml->product->count()
+			*/
+			
+			//dd($xml->product->count());
+
+			return 'teste';
+		}		
+    
+}
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    </head>
+    {{ $teste }}
+    Testando
+    <body> 
+    </body>
+</html>
