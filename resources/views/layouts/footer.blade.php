015d4b0f1f55789f7454baa8c7ca477f1a095cbf
Marcelo Rossini <mar.18.celo@gmail.com>
2016-12-14 12:50:37 -0200

    logos

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,19 @@
 <footer class="grey darken-3">
   <div class="container">
-    <div class="row">
-      <div class="col s12 m12 l12">
-        <h5 class="white-text">  © {{ date('Y') }} gm.desenvolv</h5>
+    <br>
+    <div class="row valign-wrapper hide-on-med-and-down">
+      <div class="col s12 m12 l2">
+        <img src="{{ asset('img/qv.png') }}" height="22">
       </div>
+      <div class="col s12 m12 l2 offset-l8">
+        <img src="{{ asset('img/gmdesenv.png') }}" height="30">
+      </div>
+    </div>
+    <div class="center hide-on-large-only">
+      <img src="{{ asset('img/qv.png') }}" width="80%">
+      <br>
+      <br>
+      <img src="{{ asset('img/gmdesenv.png') }}" width="80%">
     </div>
   </div>
 </footer>
